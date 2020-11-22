f9390380f327b1f6c1815bfa09e7cf549ee031b4
Luis Cordero Pinela <luis.cordero@utec.edu.pe>
2020-11-22 16:26:59 -0500

    API keys

---
+++ untitled1/params.py
@@ -0,0 +1,33 @@
+
+#Variables that contains the user credentials for the Twitter API 
+access_token = "3031693648-XiCHYV4zu6T82h86pKcsmHQ3d7ti3ImAfqNuZzZ"
+access_token_secret = "ROws8QGGSyHCnmzsHqKlawt2cqjjFwzEbpNfDqau4zzTl"
+consumer_key = "6BP5Vzrn29x1bTu4umak6ZGpY"
+consumer_secret = "9T6lFbudLg69bGan3r5sr4Z3YxjxVQXOqMrYl5xqR3lDfjfrsE"
+
+#destination folder for raw tweets 
+folder_path = "raw/"
+#destination folder for clean tweets
+select_path = "clean/"
+
+# the list of tracking words 
+tracklist = {
+'@luchocastanedap': ['@luchocastanedap', 'luis castañeda',  'Luis Castañeda Pardo'],
+'@DanielUrresti1': ['@DanielUrresti1', 'Daniel Urresti'],
+'@humberto_lay': ['@humberto_lay', 'Humberto Lay', 'Humberto Lay Sun'],
+'@RicardoBelmontC': ['@RicardoBelmontC', 'Ricardo Belmont', 'Ricardo Belmont Cassinelli'],
+'@BeingoleaA': ['@BeingoleaA', 'Alberto Beingolea'],
+'@jaimesalinas80': ['@jaimesalinas80', 'Jaime Salinas'],
+'@JorgeMunozAP': ['@JorgeMunozAP', 'Jorge Muñoz', 'Jorge Muñoz Wells'],
+'@EstherCapunay': ['@EstherCapunay', 'Esther Capuñay', 'La Capuñay'],
+'@ManuelVelardeD': ['@ManuelVelardeD', 'Manuel Velarde'],
+'@DitelColumbus': ['@DitelColumbus', 'Ditel Columbus', 'Ditel Columbus - LIMA'],
+'@ENRIQUECORNEJOR': ['@ENRIQUECORNEJOR', 'Enrique Cornejo'],
+'@juancarloszurek': ['@juancarloszurek', 'Juan Carlos Zurek'],
+'@JulioGagoPe': ['@JulioGagoPe', 'Julio Gagó Pérez', 'Julio Gagó'],
+'@Renzo_Reggiardo': ['@Renzo_Reggiardo', 'Renzo Reggiardo'],
+'@EnriquePorLima': ['@EnriquePorLima', 'Enrique Fernández Chacón'],
+'@kikeocrospoma': ['@kikeocrospoma', 'Enrique Ocrospoma'],
+'@GGG_pe': ['@GGG_pe', 'Gustavo Guerra García'],
+'@GomezBacaxLima': ['@GomezBacaxLima', 'Roberto Gómez Baca']
+}
