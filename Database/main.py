b17b3c2da59a5dfe61c0bce69adea0bf7f61aa27
Victor <Ascua>
2020-11-25 20:54:39 -0500

    Query

--- Database/main.py
+++ Database/main.py
@@ -3,9 +3,12 @@ from os.path import isfile, join
 
 from Database.Files import setFiles
 from Database.invertindex import tf_idf
+from Database.query import query
 
 files = [i for i in listdir("clean/") if isfile(join("clean/", i))]
 
 if __name__ == '__main__':
-    tweet_text = setFiles(files)
-    tf_idf(tweet_text)
+    #tweet_text = setFiles(files)
+    #tf_idf(tweet_text)
+    Result = query("Comer es sano")
+    print(Result)
--- Database/query.py
+++ Database/query.py
@@ -1,30 +1,48 @@
+import json
 import nltk
 from nltk import collections
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 
-def get_tf(query):
-    return 1
+from Database.Files import stoplist, stemmer
 
-def get_idf(query):
-    return 1
 
 def top(K, Scores):
+    Result = {}
+    Scores_items = Scores.items()
+    Scores_sorted = sorted(Scores_items, key=lambda coche: coche[1])
+    i = 0
+    for doc in Scores_sorted:
+        if i == K:
+            break
+        Result[doc[0]] = doc[1]
+        i += 1
     return Scores
 
+def parsing(content):
+    files_tokens = nltk.word_tokenize(content)
+    file_stemmed = []
+    for token in files_tokens:
+        if token in stoplist:
+            files_tokens.remove(token)
+    for token in files_tokens:
+        file_stemmed.append(stemmer.stem(token))
+    return file_stemmed
+
 def query(content):
-    Scores = []
-    Length = []
-    for t in content.split():
-        w_tq = get_tf(t)*get_idf(t)
-        for d in data:
-            w_td = get_tf(d)
-            Scores[d] += w_td*w_tq
+    Scores = {}
+    Length = {}
+    words = parsing(content)
+    for t in words:
+        with open('index.json') as file:
+            data = json.load(file)
+        for doc in data[t]:
+            Scores.setdefault(doc, 0)
+            Scores[doc] += data[t][doc]['tf-idf']
+    with open('doc_length.json') as file:
+        doc_length = json.load(file)
+    for length in doc_length:
+        Length[length] = doc_length[length]
     for d in Scores:
-        Scores[d] = Scores/Length[d]
+        Scores[d] = Scores[d]/Length[d]
     return top(5, Scores)
-
-
-if __name__ == '__main__':
-    consulta('PyCharm')
-
